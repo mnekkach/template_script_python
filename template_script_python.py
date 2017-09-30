e14b298db49f05c276ce44b0a45e6db69e7f8057
mnekkach <mnekkach@gmail.com>
2017-09-30 13:27:03 +0200

    initial commit

---
+++ template_script_python.py
@@ -0,0 +1,18 @@
+"""
+Title        :<Name of the script/program>
+Description  :<Write a description of a script/program>
+Requierments :<Add library required>
+Installation :<Add command to install script/program>
+Author       :<Name of the author of the script/program>
+Contact      :<Add email, phone to contact the author or other person>
+
+License      :<Add terms of the license to use, modify .... script/program>
+              /* Copyright (C) 2017 <XXXXX> - All Rights Reserved
+              * You may use, distribute and modify this code under the
+              * terms of the <XXXXXX> license, which unfortunately won't be
+              * written for another century.
+              *
+              * You should have received a copy of the <XXXXX> license with
+              * this file. If not, please write to:<xxxx@email.xxx> , or visit :<http://xxxxx.xxx>
+              */
+"""
